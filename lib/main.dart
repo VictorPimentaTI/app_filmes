6c1eb0c831a85fd5bd8423f2667e49a0ec3e0445
Victor Pimenta <victorpimenta.ti@gmail.com>
2021-09-22 19:09:55 -0300

    Configurado o tema da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:app_filmes/application/bindings/application_bindings.dart';
+import 'package:app_filmes/application/ui/filmes_app_ui_config.dart';
 import 'package:app_filmes/modules/home/home_module.dart';
 import 'package:app_filmes/modules/login/login_module.dart';
 import 'package:app_filmes/modules/splash/splash_module.dart';
@@ -21,11 +22,9 @@ class MyApp extends StatelessWidget
   Widget build(BuildContext context)
   {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: FilmesAppUiConfig.title,
       initialBinding: ApplicationBindings(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: FilmesAppUiConfig.theme,
       getPages: [
         // Conceito de destruction
         ...SplashModule().routers,
